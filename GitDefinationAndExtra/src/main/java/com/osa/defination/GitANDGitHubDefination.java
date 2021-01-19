efe7a66a0cf8bc68c2ad9daa509b76ed897ab476
Sahil Dip <sahildip@Sahils-MBP.cable.rcn.com>
2021-01-19 16:07:35 -0500

    I have only one Class

---
+++ GitDefinationAndExtra/src/main/java/com/osa/defination/GitANDGitHubDefination.java
@@ -0,0 +1,14 @@
+package com.osa.defination;
+
+public class GitANDGitHubDefination {
+	
+	
+	public static void name() {
+		
+		int a =10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
